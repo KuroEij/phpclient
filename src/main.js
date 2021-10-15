82571e789734b9500d85f7997999de7cf758f082
黄杰 <huangjie@wondershare.cn>
2021-10-15 11:35:46 +0800

    fix:修改安装源

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import router from "./router"
 import axios from 'axios'
-import {saveAs} from "filesaver.js-npm"
+import {saveAs} from "file-saver"
 
 Vue.config.productionTip = false
 Vue.use(ElementUI);
